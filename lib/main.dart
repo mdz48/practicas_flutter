e90af836bd09565ded770b2124eed794f5b8d7bd
mdz48 <maximilianodiazclemen.cetis138@gmail.com>
2026-05-21 21:33:41 -0600

    validacion a gps

--- lib/main.dart
+++ lib/main.dart
@@ -90,7 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
-          mainAxisAlignment: .center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [const LoginForm()],
         ),
       ),
