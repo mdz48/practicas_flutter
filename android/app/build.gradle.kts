63618eecab13a2491d333d4013f9543966a0cbbe
AdrianGzn <233318@ids.upchiapas.edu.mx>
2026-06-15 13:22:03 -0600

    corrección de versión de flutter 1.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,6 +10,7 @@ android {
     compileSdk = 36
     ndkVersion = "27.0.12077973"
 
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
